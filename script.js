b9884b77bf86076807312f319ca493d282baa1e9
Dibyajyoti Mohanty <147069279+dibyajyotimohanty13@users.noreply.github.com>
2024-08-20 02:37:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,4 @@ let sun=document.querySelector('.icon-sun');
 let moon=document.querySelector('.icon-moon');
 theme.onclick = function(){
     document.body.classList.toggle("dark");
-    if(document.body.classList.contains("dark"))
-    {
-        sun.disabled=true;
-        moon.disabled=false;
-    }
-    else{
-        sun.disabled=false;
-        moon.disabled=true;
-    }
 }
